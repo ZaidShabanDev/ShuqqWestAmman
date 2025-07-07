a288e114a2ffcb26bd7531c80f98fdf891d3044b
Zaid shaban <122286390+Firefly0027@users.noreply.github.com>
2025-07-08 01:45:30 +0300

    adding initial tailwind.config and global styles

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "@/assets/styles/globals.css";
-import { Inter } from "next/font/google";
 import {
   APP_NAME,
   APP_DESCRIPTION,
@@ -12,8 +11,6 @@ import {
 } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -40,7 +37,7 @@ export default async function RootLayout({
   }
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={`${inter.className}`}>
+      <body>
         <NextIntlClientProvider>
           {children}
         </NextIntlClientProvider>
