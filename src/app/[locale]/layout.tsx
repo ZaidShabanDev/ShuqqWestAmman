d3c60af139b506165b4156f627338245dadd1206
Zaid shaban <122286390+Firefly0027@users.noreply.github.com>
2025-07-18 17:05:22 +0300

    adding theme provider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,16 @@
-import { routing } from '@/i18n/routing'
-import '@/styles/tailwind.css'
-import type { Metadata } from 'next'
-import { hasLocale, NextIntlClientProvider } from 'next-intl'
-import { Poppins } from 'next/font/google'
-import { notFound } from 'next/navigation'
+import { routing } from '@/i18n/routing';
+import '@/styles/tailwind.css';
+import type { Metadata } from 'next';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { Poppins } from 'next/font/google';
+import { notFound } from 'next/navigation';
+import ThemeProvider from './theme-provider';
 
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
   weight: ['300', '400', '500', '600', '700'],
-})
+});
 
 export const metadata: Metadata = {
   title: {
@@ -18,22 +19,24 @@ export const metadata: Metadata = {
   },
   description: 'Premium apartments, villas, and commercial properties for sale and rent in West Amman, Jordan.',
   keywords: ['Real Estate Amman', 'Properties Jordan', 'Apartments Amman', 'Villas West Amman'],
-}
+};
 
 export default async function RootLayout({
   children,
   params,
 }: Readonly<{ children: React.ReactNode; params: Promise<{ locale: string }> }>) {
-  const { locale } = await params
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
-    notFound()
+    notFound();
   }
 
   return (
     <html lang={locale} className={poppins.className}>
-      <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      <body className="bg-white text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
+        <ThemeProvider>
+          <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,23 @@
-import { useTranslations } from 'next-intl'
+import SwitchDarkMode from '@/shared/SwitchDarkMode';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
-  const t = useTranslations()
-  return <div>{t('hello_world')}</div>
+  const t = useTranslations();
+
+  const renderSwitchDarkMode = () => {
+    return (
+      <div className="mt-4">
+        <span className="text-sm font-medium">Dark mode</span>
+        <div className="mt-1.5">
+          <SwitchDarkMode />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {t('hello_world')} {renderSwitchDarkMode()}
+    </div>
+  );
 }
